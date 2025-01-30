6bd16718d3792a702dcb4b0beeaa51cd10a34eef
samymity <samymity@gmail.com>
2025-01-30 17:41:52 -0600

    Update HelloWorldController.java

--- src/main/java/com/example/helloworld/HelloWorldController.java
+++ src/main/java/com/example/helloworld/HelloWorldController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloWorldController {
 
-    @GetMapping("/hello")
+    @GetMapping("/page1")
     public String hello() {
         return "Hello, World!";
     }
