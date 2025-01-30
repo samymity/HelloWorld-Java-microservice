f993ec8f5845908b73aa9de4674345d10af3e1b8
samymity <samymity@gmail.com>
2025-01-30 17:46:31 -0600

    Update HelloWorldController.java

--- src/main/java/com/example/helloworld/HelloWorldController.java
+++ src/main/java/com/example/helloworld/HelloWorldController.java
@@ -1,5 +1,8 @@
 package com.example.helloworld;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,7 +10,25 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloWorldController {
 
     @GetMapping("/page1")
-    public String hello() {
+    public String hello1() {
         return "Hello, World!";
     }
+    
+    
+    @GetMapping("/page2")
+    public Map<String, String> hello2() {
+        Map<String, String> response = new HashMap<>();
+        response.put("message", "Hello, World!");
+        return response;
+    }
+    
+    @GetMapping("/page3")
+    public Map<String, Object> hello3() {
+        Map<String, Object> response = new HashMap<>();
+        response.put("message", "Hello from page 3!");
+        response.put("pageNumber", 3);
+        response.put("status", "success");
+        return response;
+    }
+    
 }
